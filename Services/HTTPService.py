114f1f82bc120a407a9a1fd0c22149cc1cce7053
nanoTech Corp <thiagofigcosta@hotmail.com>
2018-11-22 12:19:50 -0200

    fixed httpservice constructor, need to fix url

--- Services/HTTPService.py
+++ Services/HTTPService.py
@@ -96,89 +96,92 @@ class URL(object):
 			self.resource=rawurl[lidx+1:]
 
 class HTTP(object):
-	def __init__(self, requestdata):
-		self.type=None
-		self.url=None
-		self.version=None
-		self.data=None
-		self.useragent=None
-		self.contenttype=None
-		self.contentlength=None
-		self.host=None
-		self.accept=None
-		self.acceptlanguage=None
-		self.acceptencoding=None
-		self.connection=None
-		self.status=None
-		self.server=None
+	def __init__(self, requestdata=None, status=None, data=None, contenttype=None):
+		if requestdata and not status and not data and not contenttype:
+			self.type=None
+			self.url=None
+			self.version=None
+			self.data=None
+			self.useragent=None
+			self.contenttype=None
+			self.contentlength=None
+			self.host=None
+			self.accept=None
+			self.acceptlanguage=None
+			self.acceptencoding=None
+			self.connection=None
+			self.status=None
+			self.server=None
 
-		lines=requestdata.split('\n')
-		nlines=len(lines)
-		httpHeader=False
-		blankSpace=False # https://www.youtube.com/watch?v=e-ORhEE9VVg
-		for i in range(nlines):
-			line=lines[i]
-			if not httpHeader:
-				if "HTTP" in line:
-					method, url, version=line.split(' ')
-					if "GET" in method:
-						self.type=HTTPType.GET
-						requesttype=True
-					elif "POST" in method:
-						self.type=HTTPType.POST
-						requesttype=True
-					else:
-						print ("erro") # TODO
-					self.url=URL(url)
-					self.version=float(version.split('/')[1])
-					httpHeader=True
-
-			elif not blankSpace:
-				if line=="" and self.type==HTTPType.POST:
-					blankSpace=True
-					self.data=""
-				else:
-					idxOfSeparator=line.find(":")
-					if idxOfSeparator>0:
-						field=line[:idxOfSeparator].strip()
-						value=line[(idxOfSeparator+1):].strip()
-						if field=="User-Agent":
-							self.useragent=value
-						elif field=="Content-Type":
-							self.contenttype=value
-						elif field=="Content-Length":
-							self.contentlength=value
-						elif field=="Host":
-							self.host=value
-						elif field=="Accept":
-							self.accept=value
-						elif field=="Accept-Language":
-							self.acceptlanguage=value
-						elif field=="Accept-Enconding":
-							self.acceptencoding=value
-						elif field=="Connection":
-							self.connection=value
+			lines=requestdata.split('\n')
+			nlines=len(lines)
+			httpHeader=False
+			blankSpace=False # https://www.youtube.com/watch?v=e-ORhEE9VVg
+			for i in range(nlines):
+				line=lines[i]
+				if not httpHeader:
+					if "HTTP" in line:
+						method, url, version=line.split(' ')
+						if "GET" in method:
+							self.type=HTTPType.GET
+							requesttype=True
+						elif "POST" in method:
+							self.type=HTTPType.POST
+							requesttype=True
 						else:
-							print ("Unimplemented field: "+field)
-			else:
-				self.data+=line+'\n'
+							print ("erro") # TODO
+						self.url=URL(url)
+						self.version=float(version.split('/')[1])
+						httpHeader=True
 
-	# def __init__(self, status, data, contenttype=None):
-	# 	self.url=None
-	# 	self.useragent=None
-	# 	self.contentlength=None
-	# 	self.host=None
-	# 	self.accept=None
-	# 	self.acceptlanguage=None
-	# 	self.acceptencoding=None
-	# 	self.connection=None
+				elif not blankSpace:
+					if line=="" and self.type==HTTPType.POST:
+						blankSpace=True
+						self.data=""
+					else:
+						idxOfSeparator=line.find(":")
+						if idxOfSeparator>0:
+							field=line[:idxOfSeparator].strip()
+							value=line[(idxOfSeparator+1):].strip()
+							if field=="User-Agent":
+								self.useragent=value
+							elif field=="Content-Type":
+								self.contenttype=value
+							elif field=="Content-Length":
+								self.contentlength=value
+							elif field=="Host":
+								self.host=value
+							elif field=="Accept":
+								self.accept=value
+							elif field=="Accept-Language":
+								self.acceptlanguage=value
+							elif field=="Accept-Enconding":
+								self.acceptencoding=value
+							elif field=="Connection":
+								self.connection=value
+							else:
+								print ("Unimplemented field: "+field)
+				else:
+					self.data+=line+'\n'
+		elif not requestdata:
+			self.url=None
+			self.useragent=None
+			self.contentlength=None
+			self.host=None
+			self.accept=None
+			self.acceptlanguage=None
+			self.acceptencoding=None
+			self.connection=None
 
-	# 	self.type=HTTPType.Response
-	# 	self.status=status
-	# 	self.contenttype=contenttype
-	# 	self.data=data
-	# 	self.version=1.1
-	# 	self.server="nanoTech Pure HTTP PyServer - "+socket.gethostname()
+			self.type=HTTPType.Response
+			self.status=status
+			self.contenttype=contenttype
+			self.data=data
+			self.version=1.1
+			self.server="nanoTech Pure HTTP PyServer - "+socket.gethostname()
+		else:
+			print ("erro") # TODO
+
 
 	def toString():
 		# TODO fazer metodo para gerar string do request a partir das variaveis de self disponiveis
